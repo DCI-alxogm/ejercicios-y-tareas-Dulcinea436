a5f2a7f7c4732d198a8278136b1a8c46116523ce
Dulcinea436 <dm.gonzalezpantaleon@ugto.mx>
2025-01-29 11:30:18 -0600

    Mi primer comentario en commit

---
+++ Jan29/programa_hola.c
@@ -0,0 +1,8 @@
+/*
+
+Esto es un comentario. Me sirve para escribir información relevante del programa que estoy haciendo. Por 
+ejemplo:
+Este programa imprimirá la típica frase “hola mundo” a la pantalla
+Creado el 29/01/2025 por Dulce Maria 
+
+*/
