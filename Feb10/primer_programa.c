1822194038dc2e799b5ae0e487a91db2acc9e89c
Dulcinea436 <dm.gonzalezpantaleon@ugto.mx>
2025-02-10 11:35:36 -0600

    10 de Febrero

---
+++ Feb10/primer_programa.c
@@ -0,0 +1,12 @@
+/*Creado el 10 de Febrero del 2025 por dulce gonzalez*/
+#include<stdio.h>
+
+int main()
+{
+
+printf("Hola Dulce \n")
+primtf ("Este es el primer programa del curso\n");
+
+}
+
+/*Termine de escribir el programa*/
