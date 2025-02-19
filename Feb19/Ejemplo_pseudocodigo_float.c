c7300f20e046ff22e75f037172628da05e89140d
Dulcinea436 <dm.gonzalezpantaleon@ugto.mx>
2025-02-19 11:08:42 -0600

    Ejercicio de pseudocodigo de 19 de Febrero

---
+++ Feb19/Ejemplo_pseudocodigo_float.c
@@ -0,0 +1,15 @@
+/*
+19 de Febero del 2025
+*/
+
+#include <stdio.h> 
+#include <stdlib.h>
+
+int main(){
+     float TC, TK; //
+     printf("TC=36\n");
+     scanf("%f", &TC);
+     TK= TC + 273.15;
+     printf ("La temperatura en Kelvin es : %f \n",TK);
+     exit (0);
+}
