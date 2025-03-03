92b5ed488f31f5052e7a5caaba1e2ef051e8f195
Dulcinea436 <dm.gonzalezpantaleon@ugto.mx>
2025-03-03 11:52:23 -0600

    Ejercicio corregido

---
+++ Mar3/Ejercicio_correccion.c
@@ -0,0 +1,35 @@
+/* 
+Ejercicio corregido el 3 de Marzo del 2025
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main (){
+              int numero,residuo;
+              char opc[2];
+
+
+             leernumero:
+                         printf("Introduce el numero a revisar");
+                         scanf("%i",&numero);
+                         residuo=numero%2;
+                         if(residuo==0){
+                         printf("el numero %i es par\n",numero);
+                          }else{
+                         printf("el numero %i es impar\n",numero);
+                         }
+                         printf("¿Quieres revisar otro numero?\n");
+                         scanf("%s",&opc);
+
+                         if (strcmp(opc,"si")==0){
+                                goto leernumero;
+                         }else if (strcmp(opc, "no")==1){
+                                printf("hasta la proxima\n");
+                         }else{
+                                printf("solo acepto si o no");
+                         }
+
+}
+
+
