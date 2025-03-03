104f21251c7598f572c499bc9cd85ff08c477e81
Dulcinea436 <dm.gonzalezpantaleon@ugto.mx>
2025-03-02 19:29:05 -0600

    Prueba del 3 de marzo

---
+++ Marzo3/Prueba_nueva.c
@@ -0,0 +1,3 @@
+/* Prueba desl 3 de Marzo del 2025
+Esta prueba se esta subiendo desde mi hogar, ya con todo lo requerido instalado, habiendo instalado ubuntu, ya teniendo linux, ya teniendo clonado el repositorio de GitHub, lista para subir tareas
+*/
