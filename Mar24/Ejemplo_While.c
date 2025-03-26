b7abcb6a513797c9e7489134ce766058fe2270b4
Dulcinea436 <dm.gonzalezpantaleon@ugto.mx>
2025-03-25 22:18:28 -0600

    Primer Ejercicio con el comando While

---
+++ Mar24/Ejemplo_While.c
@@ -0,0 +1,25 @@
+/* 
+Ejemplo con la instrucción de While
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    float temp_C,temp_K;
+    float inicial=100, final=200,delta;
+    int n=10;
+    
+    delta=(final-inicial)/n;
+    temp_C=inicial;
+    
+    while(temp_C<=final){
+        temp_K=temp_C+273.15;
+        printf("%f %f",temp_C, temp_K);
+        temp_C=temp_C+delta;
+        
+    }
+    exit(0);
+}
